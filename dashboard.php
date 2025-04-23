d61df891217f3399d893ec0555b0d388ced3f229
JORDAN\cocdr <nutkoko675@gmail.com>
2025-04-23 21:19:13 +0800

    last push

--- dashboard.php
+++ dashboard.php
@@ -99,12 +99,11 @@ $conn->close();
                 <h2 class="text-green-700 text-xl">Alive Plants/Trees</h2>
                 <p class="text-gray-600">You have <?php echo $alive_count; ?> Alive Plants/Trees.</p>
             </a>
-            <a href="pages/viewlistdead.php" class=" bg-gray-100 border border-gray-300 rounded-lg p-4 text-center shadow hover:shadow-lg transition -transform transform hover:scale-105">
-                <h2 class="text-green-700 text-xl">Dead Plants/Trees</h2>
+            <a href="pages/viewlistdead.php" class="bg-gray-100 border border-gray-300 rounded-lg p-4 text-center shadow hover:shadow-lg transition-transform transform hover:scale-105">
+                <h2 class="text-[rgba(255,0,0,1)] text-xl">Dead Plants/Trees</h2>
                 <p class="text-gray-600">You have <?php echo $dead_count; ?> Dead Plants/Trees.</p>
             </a>
         </div>
-
         <div class="flex flex-col md:flex-row justify-between">
             <div class="w-full md:w-1/2 mb-6 md:mb-0">
                 <div class="bg-white rounded-lg shadow p-4">
